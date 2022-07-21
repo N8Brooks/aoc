039aa85655612ce91f1a84fe1b53936d65507b9a
Nathan Brooks <natebrooks004@gmail.com>
2022-07-20 19:34:26 -0700

    fix(year_2017): day 11 snake case

--- src/year_2017/day_11.rs
+++ src/year_2017/day_11.rs
@@ -16,11 +16,11 @@ fn distance(c: Complex<i32>) -> i32 {
     (c.im.abs() + (c.im + c.re).abs() + c.re.abs()) / 2
 }
 
-pub fn part1(input: &str) -> i32 {
+pub fn part_1(input: &str) -> i32 {
     distance(input.split(',').map(direction).sum())
 }
 
-pub fn part2(input: &str) -> i32 {
+pub fn part_2(input: &str) -> i32 {
     input
         .split(',')
         .map(direction)
@@ -35,58 +35,58 @@ pub fn part2(input: &str) -> i32 {
 
 #[cfg(test)]
 mod tests {
-    use super::{part1, part2};
+    use super::{part_1, part_2};
     use std::fs::read_to_string;
 
     #[test]
     fn part_1_example_1() {
-        assert_eq!(part1("ne,ne,ne"), 3);
+        assert_eq!(part_1("ne,ne,ne"), 3);
     }
 
     #[test]
     fn part_1_example_2() {
-        assert_eq!(part1("ne,ne,sw,sw"), 0);
+        assert_eq!(part_1("ne,ne,sw,sw"), 0);
     }
 
     #[test]
     fn part_1_example_3() {
-        assert_eq!(part1("ne,ne,s,s"), 2);
+        assert_eq!(part_1("ne,ne,s,s"), 2);
     }
 
     #[test]
     fn part_1_example_4() {
-        assert_eq!(part1("se,sw,se,sw,sw"), 3);
+        assert_eq!(part_1("se,sw,se,sw,sw"), 3);
     }
 
     #[test]
     fn part_1_input() {
         let input = read_to_string("src/year_2017/testdata/day_11.txt").unwrap();
-        assert_eq!(part1(&input), 784);
+        assert_eq!(part_1(&input), 784);
     }
 
     #[test]
     fn part_2_example_1() {
-        assert_eq!(part2("ne,ne,ne"), 3);
+        assert_eq!(part_2("ne,ne,ne"), 3);
     }
 
     #[test]
     fn part_2_example_2() {
-        assert_eq!(part2("ne,ne,sw,sw"), 2);
+        assert_eq!(part_2("ne,ne,sw,sw"), 2);
     }
 
     #[test]
     fn part_2_example_3() {
-        assert_eq!(part2("ne,ne,s,s"), 2);
+        assert_eq!(part_2("ne,ne,s,s"), 2);
     }
 
     #[test]
     fn part_2_example_4() {
-        assert_eq!(part2("se,sw,se,sw,sw"), 3);
+        assert_eq!(part_2("se,sw,se,sw,sw"), 3);
     }
 
     #[test]
     fn part_2_input() {
         let input = read_to_string("src/year_2017/testdata/day_11.txt").unwrap();
-        assert_eq!(part2(&input), 1558);
+        assert_eq!(part_2(&input), 1558);
     }
 }
