bef00c268dca76c4a560fbf7474b1dcfd8a4d12f
Nathan Brooks <nathan@Nathans-MacBook-Pro.local>
2023-10-09 10:47:36 -0500

    feat(go): year 2022 day 4

---
+++ go/year_2022/day_04/day_04_test.go
@@ -0,0 +1,45 @@
+package year_2022
+
+import (
+	"testing"
+	"util"
+)
+
+const Example = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
+
+func TestPart1(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected int
+	}{
+		{Example, 2},
+		{util.Input(2022, 4), 540},
+	}
+	for i, c := range cases {
+		actual := Part1(c.input)
+		if actual != c.expected {
+			t.Errorf("%d: actual %d, expected %d", i, actual, c.expected)
+		}
+	}
+}
+
+func TestPart2(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected int
+	}{
+		{Example, 4},
+		{util.Input(2022, 4), 872},
+	}
+	for i, c := range cases {
+		actual := Part2(c.input)
+		if actual != c.expected {
+			t.Errorf("%d: actual %d, expected %d", i, actual, c.expected)
+		}
+	}
+}
