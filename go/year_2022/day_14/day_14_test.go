44c27e04f5b207282b6822dabe7d3c70b7d0007a
SkookumChoocher <natebrooks004@gmail.com>
2023-10-27 11:54:39 -0500

    feat(go): year 2022 day 14

---
+++ go/year_2022/day_14/day_14_test.go
@@ -0,0 +1,41 @@
+package year_2022
+
+import (
+	"testing"
+	"util"
+)
+
+const Example = `498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9`
+
+func TestPart1(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected int
+	}{
+		{Example, 24},
+		{util.Input(2022, 14), 1133},
+	}
+	for i, c := range cases {
+		actual := Part1(c.input)
+		if actual != c.expected {
+			t.Errorf("%d: actual %d, expected %d", i, actual, c.expected)
+		}
+	}
+}
+
+func TestPart2(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected int
+	}{
+		{Example, 93},
+		{util.Input(2022, 14), 27566},
+	}
+	for i, c := range cases {
+		actual := Part2(c.input)
+		if actual != c.expected {
+			t.Errorf("%d: actual %d, expected %d", i, actual, c.expected)
+		}
+	}
+}
