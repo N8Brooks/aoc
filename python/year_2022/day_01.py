5b11d68ab99d7c5bf3d73ce0d94f4683337b767c
SkookumChoocher <natebrooks004@gmail.com>
2023-11-30 20:44:14 -0600

    feat(python): year 2022 day 1

---
+++ python/year_2022/day_01.py
@@ -0,0 +1,54 @@
+from typing import Iterable
+from heapq import nlargest
+
+
+def part_1(input: str) -> int:
+    return max(iter_total_calories(input))
+
+
+def part_2(input: str) -> int:
+    return sum(nlargest(3, iter_total_calories(input)))
+
+
+def iter_total_calories(input: str) -> Iterable[int]:
+    """Get total calories for each elf"""
+    return map(sum_calories, input.split("\n\n"))
+
+
+def sum_calories(input: str) -> int:
+    """Sum of calories for an elf"""
+    return sum(map(int, input.splitlines()))
+
+
+EXAMPLE = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+
+def test_part_1_example():
+    assert part_1(EXAMPLE) == 24000
+
+
+def test_part_1_input():
+    with open("../testdata/year_2022/day_01.txt", "r") as f:
+        assert part_1(f.read()) == 68802
+
+
+def test_part_2_example():
+    assert part_2(EXAMPLE) == 45000
+
+
+def test_part_2_input():
+    with open("../testdata/year_2022/day_01.txt", "r") as f:
+        assert part_2(f.read()) == 205370
