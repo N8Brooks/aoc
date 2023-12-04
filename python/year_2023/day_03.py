868e9a066549b37a393bd8d2578381cd235c1752
SkookumChoocher <natebrooks004@gmail.com>
2023-12-03 19:16:45 -0600

    refactor(python) year 2023 day 3

--- python/year_2023/day_03.py
+++ python/year_2023/day_03.py
@@ -20,7 +20,7 @@ def part_1(input: str) -> int:
                 ((i, j1), (i, j2 - 1)),
                 zip(repeat(i + 1), range(j1, j2)),
             )
-            if any((i3, j3) in symbols for i3, j3 in surrounding):
+            if any((i, j) in symbols for i, j in surrounding):
                 total += int(number[0])
     return total
 
@@ -28,24 +28,24 @@ def part_1(input: str) -> int:
 def part_2(input: str) -> int:
     lines = input.rstrip().splitlines()
     total = 0
-    numbers = {}
-    for i, line in enumerate(lines):
+    all_nums = {}
+    for i1, line in enumerate(lines):
         for match in re.finditer(r"\d+", line):
-            domain = zip(repeat(i), range(match.start(), match.end()))
-            numbers |= dict.fromkeys(domain, match)
-    for i, line in enumerate(lines):
-        for j, char in enumerate(line):
+            domain = zip(repeat(i1), range(match.start(), match.end()))
+            all_nums |= dict.fromkeys(domain, match)
+    for i1, line in enumerate(lines):
+        for j2, char in enumerate(line):
             if char != "*":
                 continue
-            matches = []
-            for i1, j1 in product(range(i - 1, i + 2), range(j - 1, j + 2)):
-                number = numbers.get((i1, j1), None)
-                if number is None or any(number is match for match in matches):
+            nums = []
+            for i2, j2 in product(range(i1 - 1, i1 + 2), range(j2 - 1, j2 + 2)):
+                num = all_nums.get((i2, j2), None)
+                if num is None or num in nums:
                     continue
-                matches.append(number)
-            if len(matches) == 2:
-                a, b = matches
-                total += int(a[0]) * int(b[0])
+                nums.append(num)
+            if len(nums) == 2:
+                num1, num2 = nums
+                total += int(num1[0]) * int(num2[0])
     return total
 
 
