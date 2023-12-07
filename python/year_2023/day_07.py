a87b0d06d8e9ed443459c5e679a069e3d73407b5
SkookumChoocher <natebrooks004@gmail.com>
2023-12-07 13:11:38 -0600

    refactor(python): year 2023 day 7

--- python/year_2023/day_07.py
+++ python/year_2023/day_07.py
@@ -1,58 +1,46 @@
-from collections import Counter
 from typing import Callable
 from functools import reduce
-
-
-def total_winnings(input: str, rank: Callable[[str], int]) -> int:
-    hand_bids = (line.split(" ") for line in input.rstrip().splitlines())
-    hand_bids = [(rank(hand), int(bid)) for hand, bid in hand_bids]
-    hand_bids.sort(key=lambda hand_bid: hand_bid[0])
-    return sum(rank * bid for rank, (_, bid) in enumerate(hand_bids, 1))
+from heapq import nlargest
 
 
 def part_1(input: str) -> int:
-    return total_winnings(input, rank_1)
+    return total_winnings(input, hand_strength_1)
 
 
-def rank_1(hand: str) -> int:
-    counter = Counter(hand)
-    counts = iter(sorted(counter.values(), reverse=True))
-    mode_1 = next(counts)
-    mode_2 = next(counts, 0)
-    hand_type = HAND_TYPES_1.index((mode_1, mode_2))
-    return reduce(lambda a, b: a * 13 + b, map(CARDS_1.index, hand), hand_type)
+def hand_strength_1(hand: str) -> int:
+    counts = map(hand.count, CARDS_1)
+    count_1, count_2 = nlargest(2, counts)
+    hand_type_strength = HANDS.index((count_1, count_2))
+    card_strengths = map(CARDS_1.index, hand)
+    return reduce(lambda a, b: a * 13 + b, card_strengths, hand_type_strength)
 
 
 CARDS_1 = "23456789TJQKA"
 
-HAND_TYPES_1 = [
-    (1, 1),  # High Card
-    (2, 1),  # One Pair
-    (2, 2),  # Two Pair
-    (3, 1),  # Three of a Kind
-    (3, 2),  # Full House
-    (4, 1),  # Four of a Kind
-    (5, 0),  # Five of a Kind
-]
-
 
 def part_2(input: str) -> int:
-    return total_winnings(input, rank_2)
+    return total_winnings(input, hand_strength_2)
 
 
-def rank_2(hand: str) -> int:
-    counter = Counter(hand)
-    wildcards = counter.pop("J", 0)
-    counts = iter(sorted(counter.values(), reverse=True))
-    mode_1 = next(counts, 0) + wildcards
-    mode_2 = next(counts, 0)
-    hand_type = HAND_TYPES_2.index((mode_1, mode_2))
-    return reduce(lambda a, b: a * 13 + b, map(CARDS_2.index, hand), hand_type)
+def hand_strength_2(hand: str) -> int:
+    counts = map(hand.count, CARDS_2)
+    wildcards = next(counts)  # J
+    count_1, count_2 = nlargest(2, counts)
+    hand_type_strength = HANDS.index((count_1 + wildcards, count_2))
+    card_strengths = map(CARDS_2.index, hand)
+    return reduce(lambda a, b: a * 13 + b, card_strengths, hand_type_strength)
 
 
 CARDS_2 = "J23456789TQKA"
 
-HAND_TYPES_2 = [
+
+def total_winnings(input: str, hand_strength: Callable[[str], int]) -> int:
+    hand_bids = (line.split(" ") for line in input.rstrip().splitlines())
+    ranked_bids = sorted(hand_bids, key=lambda hand_bid: hand_strength(hand_bid[0]))
+    return sum(rank * int(bid) for rank, (_, bid) in enumerate(ranked_bids, 1))
+
+
+HANDS = [
     (1, 1),  # High Card
     (2, 1),  # One Pair
     (2, 2),  # Two Pair
