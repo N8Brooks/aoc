f23605bb47af40be9e76142889f3a89d70680507
SkookumChoocher <natebrooks004@gmail.com>
2023-12-13 19:19:51 -0600

    refactor(python): year 2023 day 13 nits

--- python/year_2023/day_13.py
+++ python/year_2023/day_13.py
@@ -1,43 +1,37 @@
-from typing import Iterable, Optional
 from operator import eq, ne
+from typing import Iterable, Optional
 
 
 def part_1(input: str) -> int:
-    return sum(map(reflection_1, input.rstrip().split("\n\n")))  # type: ignore
+    return sum(map(reflection_1, input.split("\n\n")))  # type: ignore
 
 
 def reflection_1(input: str) -> Optional[int]:
     rows = input.splitlines()
-    m = len(rows)
-    for i in range(1, m):
+    for i in range(1, len(rows)):
         if all(map(eq, rows[i - 1 :: -1], rows[i:])):
             return i * 100
 
-    cols = list(zip(*rows))
-    n = len(cols)
-    for j in range(1, n):
+    cols = tuple(zip(*rows))
+    for j in range(1, len(cols)):
         if all(map(eq, cols[j - 1 :: -1], cols[j:])):
             return j
 
 
 def part_2(input: str) -> int:
-    return sum(map(reflection_2, input.rstrip().split("\n\n")))  # type: ignore
+    return sum(map(reflection_2, input.split("\n\n")))  # type: ignore
 
 
 def reflection_2(input: str) -> Optional[int]:
     rows = input.splitlines()
     for i in range(1, len(rows)):
-        if sum_diffs_2d(rows[i - 1 :: -1], rows[i:]) == 1:
+        if sum(map(sum_diffs_1d, rows[i - 1 :: -1], rows[i:])) == 1:
             return i * 100
 
     cols = tuple(zip(*rows))
     for j in range(1, len(cols)):
-        if sum_diffs_2d(cols[j - 1 :: -1], cols[j:]) == 1:
+        if sum(map(sum_diffs_1d, cols[j - 1 :: -1], cols[j:])) == 1:
             return j
-
-
-def sum_diffs_2d(a: Iterable[Iterable[str]], b: Iterable[Iterable[str]]) -> int:
-    return sum(map(sum_diffs_1d, a, b))
 
 
 def sum_diffs_1d(a: Iterable[str], b: Iterable[str]) -> int:
@@ -50,7 +44,7 @@ def test_part_1_example_1():
 
 def test_part_1_input():
     with open("../testdata/year_2023/day_13.txt", "r") as f:
-        assert part_1(f.read()) == 41859
+        assert part_1(f.read().rstrip()) == 41859
 
 
 def test_part_2_example_1():
@@ -59,7 +53,7 @@ def test_part_2_example_1():
 
 def test_part_2_input():
     with open("../testdata/year_2023/day_13.txt", "r") as f:
-        assert part_2(f.read()) == 30842
+        assert part_2(f.read().rstrip()) == 30842
 
 
 EXAMPLE_1 = """#.##..##.
