027e8dd8137946b0977e89e365cc4faa915b6139
SkookumChoocher <natebrooks004@gmail.com>
2024-12-08 15:32:31 -0600

    refactor(rust/year_2024): day_08

--- rust/year_2024/src/day_08.rs
+++ rust/year_2024/src/day_08.rs
@@ -1,44 +1,53 @@
+use hashbrown::HashSet;
+
 use itertools::Itertools as _;
+
+type IterAxis = [Box<dyn Iterator<Item = usize>>; 2];
 
 pub fn part_1(input: &str) -> usize {
     let (antennas, n) = parse_input(input);
-    let inc_range = |x1: usize, x2: usize, step: usize| -> [Box<dyn Iterator<Item = usize>>; 2] {
-        [
-            Box::new((x1.checked_sub(step)).into_iter()),
-            Box::new((x2 + step < n).then(|| x2 + step).into_iter()),
-        ]
+    let iter_axis = |x1: usize, x2: usize| -> IterAxis {
+        if x2 > x1 {
+            let step = x2 - x1;
+            [
+                Box::new((x1.checked_sub(step)).into_iter()),
+                Box::new((x2 + step < n).then(|| x2 + step).into_iter()),
+            ]
+        } else {
+            let step = x1 - x2;
+            [
+                Box::new((x1 + step < n).then(|| x1 + step).into_iter()),
+                Box::new(x2.checked_sub(step).into_iter()),
+            ]
+        }
     };
-    let dec_range = |x1: usize, x2: usize, step: usize| -> [Box<dyn Iterator<Item = usize>>; 2] {
-        [
-            Box::new((x1 + step < n).then(|| x1 + step).into_iter()),
-            Box::new(x2.checked_sub(step).into_iter()),
-        ]
-    };
-    count_antinodes(antennas, inc_range, dec_range)
+    count_antinodes(antennas, iter_axis)
 }
 
 pub fn part_2(input: &str) -> usize {
     let (antennas, n) = parse_input(input);
-    let inc_range = |x1: usize, x2: usize, step: usize| -> [Box<dyn Iterator<Item = usize>>; 2] {
-        [
-            Box::new((0..=x1).rev().step_by(step)),
-            Box::new((x2..n).step_by(step)),
-        ]
+    let iter_axis = |x1: usize, x2: usize| -> IterAxis {
+        if x2 > x1 {
+            let step = x2 - x1;
+            [
+                Box::new((0..=x1).rev().step_by(step)),
+                Box::new((x2..n).step_by(step)),
+            ]
+        } else {
+            let step = x1 - x2;
+            [
+                Box::new((x1..n).step_by(step)),
+                Box::new((0..=x2).rev().step_by(step)),
+            ]
+        }
     };
-    let dec_range = |x1: usize, x2: usize, step: usize| -> [Box<dyn Iterator<Item = usize>>; 2] {
-        [
-            Box::new((x1..n).step_by(step)),
-            Box::new((0..=x2).rev().step_by(step)),
-        ]
-    };
-    count_antinodes(antennas, inc_range, dec_range)
+    count_antinodes(antennas, iter_axis)
 }
 
 fn parse_input(input: &str) -> (impl Iterator<Item = Vec<(usize, usize)>>, usize) {
-    let mat = input.lines().collect_vec();
-    let n = mat.len(); // Assume square matrix
-    let group_map = mat
-        .into_iter()
+    let n = input.lines().next().unwrap().len(); // Assume square matrix
+    let group_map = input
+        .lines()
         .enumerate()
         .flat_map(|(i, line)| line.bytes().enumerate().map(move |(j, c)| (c, (i, j))))
         .filter(|(c, _)| *c != b'.')
@@ -49,31 +58,20 @@ fn parse_input(input: &str) -> (impl Iterator<Item = Vec<(usize, usize)>>, usize
 
 fn count_antinodes(
     antennas: impl Iterator<Item = Vec<(usize, usize)>>,
-    inc_range: impl Fn(usize, usize, usize) -> [Box<dyn Iterator<Item = usize>>; 2],
-    dec_range: impl Fn(usize, usize, usize) -> [Box<dyn Iterator<Item = usize>>; 2],
+    iter_axis: impl Fn(usize, usize) -> IterAxis,
 ) -> usize {
     antennas
         .flat_map(|v| {
             v.into_iter()
                 .tuple_combinations()
                 .flat_map(|((i1, j1), (i2, j2))| {
-                    let [is1, is2] = if i2 > i1 {
-                        inc_range(i1, i2, i2 - i1)
-                    } else {
-                        dec_range(i1, i2, i1 - i2)
-                    };
-                    let [js1, js2] = if j2 > j1 {
-                        inc_range(j1, j2, j2 - j1)
-                    } else {
-                        dec_range(j1, j2, j1 - j2)
-                    };
-                    [is1.into_iter().zip(js1), is2.into_iter().zip(js2)]
-                        .into_iter()
-                        .flatten()
+                    let [is1, is2] = iter_axis(i1, i2);
+                    let [js1, js2] = iter_axis(j1, j2);
+                    is1.zip(js1).chain(is2.zip(js2))
                 })
         })
-        .unique()
-        .count()
+        .collect::<HashSet<_>>()
+        .len()
 }
 
 #[cfg(test)]
@@ -82,7 +80,8 @@ mod test {
 
     const INPUT: &str = include_str!("../../../testdata/year_2024/day_08.txt");
 
-    const EXAMPLE: &str = "............
+    const EXAMPLE: &str = "\
+............
 ........0...
 .....0......
 .......0....
