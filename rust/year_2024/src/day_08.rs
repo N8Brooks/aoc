bab9c53eacd56de7365a6c6c8e99520c027f6f58
skookumchoocher <natebrooks004@gmail.com>
2024-12-13 11:09:35 -0600

    refactor(rust/year_2024): day 8

--- rust/year_2024/src/day_08.rs
+++ rust/year_2024/src/day_08.rs
@@ -1,24 +1,16 @@
+use std::collections::HashMap;
+
 use hashbrown::HashSet;
 
 use itertools::Itertools as _;
 
-type IterAxis = [Box<dyn Iterator<Item = usize>>; 2];
-
 pub fn part_1(input: &str) -> usize {
     let (antennas, n) = parse_input(input);
-    let iter_axis = |x1: usize, x2: usize| -> IterAxis {
-        if x2 > x1 {
-            let step = x2 - x1;
-            [
-                Box::new((x1.checked_sub(step)).into_iter()),
-                Box::new((x2 + step < n).then(|| x2 + step).into_iter()),
-            ]
+    let iter_axis = |a: usize, b: usize| -> Box<dyn Iterator<Item = usize>> {
+        if a > b {
+            Box::new([a + a - b].into_iter().filter(move |c| c < &n))
         } else {
-            let step = x1 - x2;
-            [
-                Box::new((x1 + step < n).then(|| x1 + step).into_iter()),
-                Box::new(x2.checked_sub(step).into_iter()),
-            ]
+            Box::new(a.checked_sub(b - a).into_iter())
         }
     };
     count_antinodes(antennas, iter_axis)
@@ -26,48 +18,42 @@ pub fn part_1(input: &str) -> usize {
 
 pub fn part_2(input: &str) -> usize {
     let (antennas, n) = parse_input(input);
-    let iter_axis = |x1: usize, x2: usize| -> IterAxis {
-        if x2 > x1 {
-            let step = x2 - x1;
-            [
-                Box::new((0..=x1).rev().step_by(step)),
-                Box::new((x2..n).step_by(step)),
-            ]
+    let iter_axis = |a: usize, b: usize| -> Box<dyn Iterator<Item = usize>> {
+        if a > b {
+            Box::new((a..n).step_by(a - b))
         } else {
-            let step = x1 - x2;
-            [
-                Box::new((x1..n).step_by(step)),
-                Box::new((0..=x2).rev().step_by(step)),
-            ]
+            Box::new((0..=a).rev().step_by(b - a))
         }
     };
     count_antinodes(antennas, iter_axis)
 }
 
-fn parse_input(input: &str) -> (impl Iterator<Item = Vec<(usize, usize)>>, usize) {
-    let n = input.lines().next().unwrap().len(); // Assume square matrix
+fn parse_input(input: &str) -> (HashMap<u8, Vec<(usize, usize)>>, usize) {
+    let n = input.bytes().position(|c| c == b'\n').unwrap(); // Assume square matrix
     let group_map = input
         .lines()
         .enumerate()
         .flat_map(|(i, line)| line.bytes().enumerate().map(move |(j, c)| (c, (i, j))))
         .filter(|(c, _)| *c != b'.')
-        .into_group_map()
-        .into_values();
+        .into_group_map();
     (group_map, n)
 }
 
 fn count_antinodes(
-    antennas: impl Iterator<Item = Vec<(usize, usize)>>,
-    iter_axis: impl Fn(usize, usize) -> IterAxis,
+    antennas: HashMap<u8, Vec<(usize, usize)>>,
+    iter_axis: impl Fn(usize, usize) -> Box<dyn Iterator<Item = usize>>,
 ) -> usize {
     antennas
+        .into_values()
         .flat_map(|v| {
             v.into_iter()
                 .tuple_combinations()
                 .flat_map(|((i1, j1), (i2, j2))| {
-                    let [is1, is2] = iter_axis(i1, i2);
-                    let [js1, js2] = iter_axis(j1, j2);
-                    is1.zip(js1).chain(is2.zip(js2))
+                    let rows_1 = iter_axis(i1, i2);
+                    let cols_1 = iter_axis(j1, j2);
+                    let rows_2 = iter_axis(i2, i1);
+                    let cols_2 = iter_axis(j2, j1);
+                    rows_1.zip(cols_1).chain(rows_2.zip(cols_2))
                 })
         })
         .collect::<HashSet<_>>()
@@ -80,8 +66,7 @@ mod test {
 
     const INPUT: &str = include_str!("../test_data/day_08.txt");
 
-    const EXAMPLE: &str = "\
-............
+    const EXAMPLE: &str = "............
 ........0...
 .....0......
 .......0....
