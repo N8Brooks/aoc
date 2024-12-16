011558b3d589ecf00d5cacededced70a76283ecf
skookumchoocher <natebrooks004@gmail.com>
2024-12-15 18:08:37 -0600

    refactor(rust/year_2024): day 15

--- rust/year_2024/src/day_15.rs
+++ rust/year_2024/src/day_15.rs
@@ -6,47 +6,30 @@ pub fn part_1(input: &str) -> usize {
         .lines()
         .map(|line| line.bytes().collect_vec())
         .collect_vec();
-    let moves = moves.replace("\n", "");
-    let mut pos_1 = map
+    let (mut i1, mut j1) = map
         .iter()
         .enumerate()
         .find_map(|(i, line)| line.iter().position(|&c| c == b'@').map(|j| (i, j)))
         .unwrap();
-    for move_x in moves.bytes() {
-        let move_ = match move_x {
-            b'^' => |(i, j)| (i - 1, j),
-            b'v' => |(i, j)| (i + 1, j),
-            b'<' => |(i, j)| (i, j - 1),
-            b'>' => |(i, j)| (i, j + 1),
-            _ => unreachable!(),
-        };
-        let pos_2 = move_(pos_1);
-        map[pos_1.0][pos_1.1] = b'.';
-        match map[pos_2.0][pos_2.1] {
-            b'.' | b'@' => pos_1 = pos_2,
+    map[i1][j1] = b'.';
+    for move_ in moves.bytes().filter(|&c| c != b'\n').map(mover) {
+        let (i2, j2) = move_(i1, j1);
+        match map[i2][j2] {
+            b'.' => (i1, j1) = (i2, j2),
             b'O' => {
-                let mut pos_3 = pos_2;
-                while map[pos_3.0][pos_3.1] != b'#' {
-                    match map[pos_3.0][pos_3.1] {
-                        b'.' | b'@' => {
-                            map[pos_2.0][pos_2.1] = b'.';
-                            map[pos_3.0][pos_3.1] = b'O';
-                            pos_1 = pos_2;
-                            break;
-                        }
-                        b'O' => {}
-                        b'#' => {
-                            break;
-                        }
-                        _ => unreachable!(),
-                    }
-                    pos_3 = move_(pos_3);
+                let (mut i3, mut j3) = (i2, j2);
+                while map[i3][j3] == b'O' {
+                    (i3, j3) = move_(i3, j3);
+                }
+                if map[i3][j3] == b'.' {
+                    map[i2][j2] = b'.';
+                    map[i3][j3] = b'O';
+                    (i1, j1) = (i2, j2);
                 }
             }
             b'#' => {}
-            _ => unreachable!(),
+            b2 => panic!("unexpected char: {}", b2 as char),
         };
-        map[pos_1.0][pos_1.1] = b'@';
     }
     map.into_iter()
         .enumerate()
@@ -57,8 +40,15 @@ pub fn part_1(input: &str) -> usize {
 }
 
 pub fn part_2(input: &str) -> usize {
+    fn complete_box(i: usize, j: usize, b: u8) -> [(usize, usize, u8); 2] {
+        match b {
+            b'[' => [(i, j, b'['), (i, j + 1, b']')],
+            b']' => [(i, j - 1, b'['), (i, j, b']')],
+            _ => panic!("unexpected box: {}", b as char),
+        }
+    }
+
     let (map, moves) = input.split_once("\n\n").unwrap();
-    let moves = moves.replace("\n", "");
     let (mut i1, mut j1) = map
         .lines()
         .enumerate()
@@ -69,113 +59,84 @@ pub fn part_2(input: &str) -> usize {
         .map(|line| {
             line.bytes()
                 .flat_map(|b| match b {
-                    b'@' => b"@.",
+                    b'.' | b'@' => b"..",
                     b'O' => b"[]",
                     b'#' => b"##",
-                    b'.' => b"..",
-                    _ => unreachable!(),
+                    b => panic!("unexpected char: {}", b as char),
                 })
                 .copied()
                 .collect_vec()
         })
         .collect_vec();
-    for move_x in moves.bytes() {
-        let move_ = match move_x {
-            b'^' => |(i, j)| (i - 1, j),
-            b'v' => |(i, j)| (i + 1, j),
-            b'<' => |(i, j)| (i, j - 1),
-            b'>' => |(i, j)| (i, j + 1),
-            _ => unreachable!(),
-        };
-        let pos_2 = move_((i1, j1));
-        map[i1][j1] = b'.';
-        match map[pos_2.0][pos_2.1] {
-            b'.' | b'@' => (i1, j1) = pos_2,
-            b'[' | b']' if b"<>".contains(&move_x) => {
-                let mut pos_3 = pos_2;
-                while map[pos_3.0][pos_3.1] != b'#' {
-                    match map[pos_3.0][pos_3.1] {
-                        b'.' | b'@' => {
-                            map[pos_2.0][pos_2.1] = b'.';
-                            let range = if move_x == b'<' {
-                                pos_3.1..pos_2.1
-                            } else {
-                                pos_2.1 + 1..pos_3.1 + 1
-                            };
-                            range.zip(b"[]".iter().cycle()).for_each(|(j, &b)| {
-                                map[pos_3.0][j] = b;
-                            });
-                            (i1, j1) = pos_2;
-                            break;
-                        }
-                        b'[' | b']' => {}
-                        b'#' => {
-                            break;
-                        }
-                        b => {
-                            println!("{}", String::from_utf8_lossy(&[b]));
-                            unreachable!()
-                        }
-                    }
-                    pos_3 = move_(pos_3);
+
+    for dir in moves.bytes().filter(|&c| c != b'\n') {
+        let move_ = mover(dir);
+        let (i2, j2) = move_(i1, j1);
+        match map[i2][j2] {
+            b'.' => (i1, j1) = (i2, j2),
+            b'[' | b']' if dir == b'<' || dir == b'>' => {
+                let (mut i3, mut j3) = (i2, j2);
+                while b"[]".contains(&map[i3][j3]) {
+                    (i3, j3) = move_(i3, j3);
+                }
+                if map[i3][j3] == b'.' {
+                    if dir == b'<' {
+                        map[i3].copy_within(j3 + 1..j2 + 1, j3)
+                    } else {
+                        map[i3].copy_within(j2..j3, j2 + 1)
+                    };
+                    map[i2][j2] = b'.';
+                    (i1, j1) = (i2, j2);
                 }
             }
-            b @ (b'[' | b']') => {
-                let mut boxes_1 = vec![
-                    (pos_2, b),
-                    if b == b'[' {
-                        ((pos_2.0, pos_2.1 + 1), b']')
-                    } else {
-                        ((pos_2.0, pos_2.1 - 1), b'[')
-                    },
-                ];
+            b2 @ (b'[' | b']') => {
+                let mut boxes = Vec::from(complete_box(i2, j2, b2));
                 let mut idx = 0;
                 let mut success = true;
-                while idx < boxes_1.len() {
-                    let ((i1, j1), _) = boxes_1[idx];
-                    let (i2, j2) = move_((i1, j1));
-                    match map[i2][j2] {
-                        b'.' | b'@' => {}
-                        b @ (b'[' | b']') => {
-                            boxes_1.extend(&[
-                                ((i2, j2), b),
-                                if b == b'[' {
-                                    ((i2, j2 + 1), b']')
-                                } else {
-                                    ((i2, j2 - 1), b'[')
-                                },
-                            ]);
-                        }
+                while idx < boxes.len() {
+                    let (i, j, _) = boxes[idx];
+                    let (i, j) = move_(i, j);
+                    match map[i][j] {
+                        b'.' => {}
+                        b @ (b'[' | b']') => boxes.extend(complete_box(i, j, b)),
                         b'#' => {
                             success = false;
                             break;
                         }
-                        _ => unreachable!(),
+                        b => panic!("unexpected char: {}", b as char),
                     }
                     idx += 1;
                 }
                 if success {
-                    for &((i1, j1), _) in &boxes_1 {
-                        map[i1][j1] = b'.';
+                    for (i, j, b) in boxes.into_iter().rev() {
+                        map[i][j] = b'.';
+                        let (i, j) = move_(i, j);
+                        map[i][j] = b;
                     }
-                    for &(pos_1, b) in &boxes_1 {
-                        let (i2, j2) = move_(pos_1);
-                        map[i2][j2] = b;
-                    }
-                    (i1, j1) = pos_2;
+                    (i1, j1) = (i2, j2);
                 }
             }
             b'#' => {}
-            _ => unreachable!(),
+            b2 => panic!("unexpected char: {}", b2 as char),
         };
-        map[i1][j1] = b'@';
     }
+
     map.into_iter()
         .enumerate()
         .flat_map(|(i, line)| line.into_iter().enumerate().map(move |(j, c)| (i, j, c)))
         .filter(|(_, _, c)| *c == b'[')
         .map(|(i, j, _)| 100 * i + j)
         .sum()
+}
+
+fn mover(direction: u8) -> fn(usize, usize) -> (usize, usize) {
+    match direction {
+        b'^' => |i, j| (i - 1, j),
+        b'v' => |i, j| (i + 1, j),
+        b'<' => |i, j| (i, j - 1),
+        b'>' => |i, j| (i, j + 1),
+        _ => panic!("unexpected direction: {}", direction as char),
+    }
 }
 
 #[cfg(test)]
