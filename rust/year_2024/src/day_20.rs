28518de35f7f83b8d901997f6c82a9024d309b60
skookumchoocher <natebrooks004@gmail.com>
2024-12-21 00:04:48 -0600

    refactor(rust/year_2024): day 20

--- rust/year_2024/src/day_20.rs
+++ rust/year_2024/src/day_20.rs
@@ -1,16 +1,46 @@
+use std::iter::repeat;
+
 use itertools::Itertools;
 
-pub fn part_1(input: &str, save: usize) -> usize {
-    count_cheats(input, 2, save)
+pub fn part_1(input: &str) -> usize {
+    count_cheats(input, 2, 100)
 }
 
-pub fn part_2(input: &str, save: usize) -> usize {
-    count_cheats(input, 20, save)
+pub fn part_2(input: &str) -> usize {
+    count_cheats(input, 20, 100)
 }
 
-fn count_cheats(input: &str, range: isize, save: usize) -> usize {
+fn count_cheats(input: &str, range: isize, threshold: usize) -> usize {
+    let track = &parse_track(input);
+    let (m, n) = (track.len(), track[0].len());
+    track
+        .iter()
+        .enumerate()
+        .flat_map(|(i, row)| {
+            row.iter()
+                .enumerate()
+                .filter_map(move |(j, &dist)| dist.map(|dist| (i, j, dist)))
+        })
+        .flat_map(|(i1, j1, dist_1)| {
+            (-range..=range)
+                .flat_map(|di| {
+                    let dj = range - di.abs();
+                    repeat(di).zip(-dj..=dj)
+                })
+                .filter_map(move |(di, dj)| {
+                    let i2 = i1.checked_add_signed(di).filter(|&i| i < m)?;
+                    let j2 = j1.checked_add_signed(dj).filter(|&j| j < n)?;
+                    let dist_2 = track[i2][j2]? + 1;
+                    let cheat_dist = i2.abs_diff(i1) + j2.abs_diff(j1);
+                    dist_2.checked_sub(dist_1 + cheat_dist)
+                })
+        })
+        .filter(|&cheat| cheat >= threshold)
+        .count()
+}
+
+fn parse_track(input: &str) -> Vec<Vec<Option<usize>>> {
     let map = input.lines().map(|line| line.as_bytes()).collect_vec();
-    let (m, n) = (map.len(), map[0].len());
     let start = map
         .iter()
         .enumerate()
@@ -21,7 +51,6 @@ fn count_cheats(input: &str, range: isize, save: usize) -> usize {
         .enumerate()
         .find_map(|(i, row)| row.iter().position(|&b| b == b'E').map(|j| (i, j)))
         .unwrap();
-
     let (mut i1, mut j1) = (usize::MAX, usize::MAX);
     let (mut i2, mut j2) = start;
     let mut dist = 0;
@@ -32,36 +61,12 @@ fn count_cheats(input: &str, range: isize, save: usize) -> usize {
             .into_iter()
             .filter(|&(i3, j3)| (i1, j1) != (i3, j3) && map[i3][j3] != b'#')
             .map(|(i3, j3)| (i2, j2, i3, j3))
-            .exactly_one()
+            .next() // assume perfect maze
             .unwrap();
         track[i2][j2] = Some(dist);
         dist += 1;
     }
-
     track
-        .iter()
-        .enumerate()
-        .flat_map(|(i, row)| {
-            row.iter()
-                .enumerate()
-                .filter_map(move |(j, &dist)| dist.map(|dist| (i, j, dist)))
-        })
-        .flat_map(|(i1, j1, dist_1)| {
-            (-range..=range).flat_map(move |di| {
-                let dj = range - di.abs();
-                (-dj..=dj).filter_map(move |dj| {
-                    i1.checked_add_signed(di)
-                        .zip(j1.checked_add_signed(dj))
-                        .filter(|&(i2, j2)| i2 < m && j2 < n)
-                        .map(|(i2, j2)| (dist_1 + i2.abs_diff(i1) + j2.abs_diff(j1), (i2, j2)))
-                })
-            })
-        })
-        .filter_map(|(dist_1, (i2, j2))| {
-            track[i2][j2].and_then(|dist_2| dist_2.checked_sub(dist_1))
-        })
-        .filter(|&cheat| cheat + 1 >= save)
-        .count()
 }
 
 #[cfg(test)]
@@ -86,15 +91,19 @@ mod test {
 #...#...#...###
 ###############";
 
-    #[test_case(EXAMPLE, 10, 10)]
-    #[test_case(INPUT, 100, 1415)]
-    fn part_1(input: &str, save: usize, expected: usize) {
-        assert_eq!(super::part_1(input, save), expected);
+    #[test_case(INPUT, 1415)]
+    fn part_1(input: &str, expected: usize) {
+        assert_eq!(super::part_1(input), expected);
     }
 
-    #[test_case(EXAMPLE, 70, 41)]
-    #[test_case(INPUT, 100, 1022577)]
-    fn part_2(input: &str, save: usize, expected: usize) {
-        assert_eq!(super::part_2(input, save), expected);
+    #[test_case(INPUT, 1022577)]
+    fn part_2(input: &str, expected: usize) {
+        assert_eq!(super::part_2(input), expected);
+    }
+
+    #[test_case(EXAMPLE, 2, 10, 10)]
+    #[test_case(EXAMPLE, 20, 70, 41)]
+    fn count_cheats(input: &str, range: isize, threshold: usize, expected: usize) {
+        assert_eq!(super::count_cheats(input, range, threshold), expected);
     }
 }
