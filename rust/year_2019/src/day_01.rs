58b64c7cdbd953e51e8c1dd2dd7510d783b32af9
skookumchoocher <natebrooks004@gmail.com>
2025-10-03 23:36:16 -0500

    feat(year_2019): day 1

---
+++ rust/year_2019/src/day_01.rs
@@ -0,0 +1,44 @@
+pub fn part_1(input: &str) -> u32 {
+    parse_input(input).map(|mass| mass / 3 - 2).sum()
+}
+
+pub fn part_2(input: &str) -> u32 {
+    parse_input(input)
+        .map(|mut fuel| {
+            let mut total = 0;
+            while fuel >= 9 {
+                fuel = fuel / 3 - 2;
+                total += fuel;
+            }
+            total
+        })
+        .sum()
+}
+
+fn parse_input(input: &str) -> impl Iterator<Item = u32> {
+    input.lines().map(|line| line.parse().unwrap())
+}
+
+#[cfg(test)]
+mod test {
+    use test_case::test_case;
+
+    const INPUT: &str = include_str!("../test_data/day_01.txt");
+
+    #[test_case("12", 2)]
+    #[test_case("14", 2)]
+    #[test_case("1969", 654)]
+    #[test_case("100756", 33583)]
+    #[test_case(INPUT, 3408471)]
+    fn part_1(input: &str, expected: u32) {
+        assert_eq!(super::part_1(input), expected);
+    }
+
+    #[test_case("14", 2)]
+    #[test_case("1969", 966)]
+    #[test_case("100756", 50346)]
+    #[test_case(INPUT, 5109803)]
+    fn part_2(input: &str, expected: u32) {
+        assert_eq!(super::part_2(input), expected);
+    }
+}
