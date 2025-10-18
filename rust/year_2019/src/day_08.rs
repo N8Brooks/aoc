4db68ab73324910f28a56f1792bac9f794880094
skookumchoocher <natebrooks004@gmail.com>
2025-10-18 16:27:14 -0500

    use non const generic args

--- rust/year_2019/src/day_08.rs
+++ rust/year_2019/src/day_08.rs
@@ -1,9 +1,9 @@
 use itertools::Itertools as _;
 
-pub fn part_1<const M: usize, const N: usize>(input: &str) -> usize {
+pub fn part_1(input: &str, m: usize, n: usize) -> usize {
     let layer = input
         .as_bytes()
-        .chunks_exact(M * N)
+        .chunks_exact(m * n)
         .min_by_key(|layer| layer.iter().filter(|&&b| b == b'0').count())
         .unwrap();
     let count_1 = layer.iter().filter(|&&b| b == b'1').count();
@@ -11,33 +11,27 @@ pub fn part_1<const M: usize, const N: usize>(input: &str) -> usize {
     count_1 * count_2
 }
 
-pub fn part_2<const M: usize, const N: usize>(input: &str) -> String {
-    let (rows, []) = input.as_bytes().as_chunks::<N>() else {
-        panic!("Input length is not a multiple of N");
-    };
-    let (layers, []) = rows.as_chunks::<M>() else {
-        panic!("Input length is not a multiple of M * N");
-    };
-    let image = layers.iter().fold([[b'2'; N]; M], |mut layer_1, layer_2| {
-        for (row_1, row_2) in layer_1.iter_mut().zip(layer_2) {
-            for (pxl_1, pxl_2) in row_1.iter_mut().zip(row_2) {
+pub fn part_2(input: &str, m: usize, n: usize) -> String {
+    input
+        .as_bytes()
+        .chunks_exact(m * n)
+        .fold(vec![b'2'; m * n], |mut layer_1, layer_2| {
+            for (pxl_1, pxl_2) in layer_1.iter_mut().zip(layer_2) {
                 if *pxl_1 == b'2' {
                     *pxl_1 = *pxl_2;
                 }
             }
-        }
-        layer_1
-    });
-    image
-        .into_iter()
-        .map(|row| {
-            row.into_iter()
+            layer_1
+        })
+        .chunks_exact(n)
+        .map(|row| -> String {
+            row.iter()
                 .map(|pxl| match pxl {
                     b'0' => ' ',
                     b'1' => '#',
                     _ => panic!("Invalid pixel value {pxl}"),
                 })
-                .collect::<String>()
+                .collect()
         })
         .join("\n")
 }
@@ -50,22 +44,13 @@ mod test {
 
     const INPUT: &str = include_str!("../test_data/day_08.txt");
 
-    #[test_case(EXAMPLE_1, 1)]
-    fn example_1(input: &str, expected: usize) {
-        assert_eq!(super::part_1::<2, 3>(input), expected);
-    }
-
-    #[test_case(INPUT, 1920)]
-    fn part_1(input: &str, expected: usize) {
-        assert_eq!(super::part_1::<6, 25>(input), expected);
+    #[test_case(EXAMPLE_1, 1, 2, 3)]
+    #[test_case(INPUT, 1920, 6, 25)]
+    fn part_1(input: &str, expected: usize, m: usize, n: usize) {
+        assert_eq!(super::part_1(input, m, n), expected);
     }
 
     const EXAMPLE_2: &str = "0222112222120000";
-
-    #[test_case(EXAMPLE_2, " #\n# ")]
-    fn example_2(input: &str, expected: &str) {
-        assert_eq!(super::part_2::<2, 2>(input), expected.to_string());
-    }
 
     const EXPECTED_2: &str = "\
 ###   ##  #  # #     ##  
@@ -75,8 +60,9 @@ mod test {
 #    #  # #  # #    #  # 
 #     ##   ##  #### #  # ";
 
-    #[test_case(INPUT, EXPECTED_2)]
-    fn part_2(input: &str, expected: &str) {
-        assert_eq!(super::part_2::<6, 25>(input), expected.to_string());
+    #[test_case(EXAMPLE_2, " #\n# ", 2, 2)]
+    #[test_case(INPUT, EXPECTED_2, 6, 25)]
+    fn part_2(input: &str, expected: &str, m: usize, n: usize) {
+        assert_eq!(super::part_2(input, m, n), expected.to_string());
     }
 }
