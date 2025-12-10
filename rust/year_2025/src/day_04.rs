cf7e99c11acdb8c5fcb0f98f62560f2b0c547134
skookumchoocher <natebrooks004@gmail.com>
2025-12-09 22:05:44 -0600

    refactor(rust/year_2025): day 4

--- rust/year_2025/src/day_04.rs
+++ rust/year_2025/src/day_04.rs
@@ -1,42 +1,15 @@
 pub fn part_1(input: &str) -> usize {
-    let grid: Vec<Vec<_>> = input
-        .lines()
-        .map(|line| line.bytes().map(|b| b == b'@').collect())
-        .collect();
-    let (m, n) = (grid.len(), grid[0].len());
-
-    grid.iter()
-        .enumerate()
-        .flat_map(|(i, row)| {
-            row.iter()
-                .enumerate()
-                .filter(|&(_, b)| *b)
-                .map(move |(j, _)| (i, j))
-        })
-        .map(|(i, j)| neighbors(m, n, i, j).filter(|&(i, j)| grid[i][j]).count())
+    parse_counts(input)
+        .into_iter()
+        .flatten()
+        .flatten()
         .filter(|&count| count < 4)
         .count()
 }
 
 pub fn part_2(input: &str) -> usize {
-    let grid: Vec<Vec<_>> = input
-        .lines()
-        .map(|line| line.bytes().map(|b| b == b'@').collect())
-        .collect();
-    let (m, n) = (grid.len(), grid[0].len());
-
-    let mut counts: Vec<Vec<_>> = grid
-        .iter()
-        .enumerate()
-        .map(|(i, row)| {
-            row.iter()
-                .enumerate()
-                .map(|(j, &b)| {
-                    b.then(|| neighbors(m, n, i, j).filter(|&(i, j)| grid[i][j]).count())
-                })
-                .collect()
-        })
-        .collect();
+    let mut counts = parse_counts(input);
+    let (m, n) = (counts.len(), counts[0].len());
 
     let mut stack: Vec<_> = counts
         .iter()
@@ -51,7 +24,23 @@ pub fn part_2(input: &str) -> usize {
 
     let mut rolls_of_paper = 0;
     while let Some((i, j)) = stack.pop() {
-        let update = neighbors(m, n, i, j).filter(|&(ni, nj)| {
+        let im1 = i.checked_sub(1);
+        let ip1 = (i + 1 < m).then_some(i + 1);
+        let jm1 = j.checked_sub(1);
+        let jp1 = (j + 1 < n).then_some(j + 1);
+        let update = [
+            (im1, jm1),
+            (im1, Some(j)),
+            (im1, jp1),
+            (Some(i), jm1),
+            (Some(i), jp1),
+            (ip1, jm1),
+            (ip1, Some(j)),
+            (ip1, jp1),
+        ]
+        .into_iter()
+        .filter_map(|(i, j)| i.zip(j))
+        .filter(|&(ni, nj)| {
             counts[ni][nj].as_mut().is_some_and(|count| {
                 *count -= 1;
                 *count == 3
@@ -63,23 +52,42 @@ pub fn part_2(input: &str) -> usize {
     rolls_of_paper
 }
 
-fn neighbors(m: usize, n: usize, i: usize, j: usize) -> impl Iterator<Item = (usize, usize)> {
-    let im1 = i.checked_sub(1);
-    let ip1 = (i + 1 < m).then_some(i + 1);
-    let jm1 = j.checked_sub(1);
-    let jp1 = (j + 1 < n).then_some(j + 1);
-    [
-        (im1, jm1),
-        (im1, Some(j)),
-        (im1, jp1),
-        (Some(i), jm1),
-        (Some(i), jp1),
-        (ip1, jm1),
-        (ip1, Some(j)),
-        (ip1, jp1),
-    ]
-    .into_iter()
-    .filter_map(|(i, j)| i.zip(j))
+fn parse_counts(input: &str) -> Vec<Vec<Option<usize>>> {
+    let grid: Vec<Vec<_>> = input
+        .lines()
+        .map(|line| line.bytes().map(|b| b == b'@').collect())
+        .collect();
+    let (m, n) = (grid.len(), grid[0].len());
+    grid.iter()
+        .enumerate()
+        .map(|(i, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(j, &b)| {
+                    b.then(|| {
+                        let im1 = i.checked_sub(1);
+                        let ip1 = (i + 1 < m).then_some(i + 1);
+                        let jm1 = j.checked_sub(1);
+                        let jp1 = (j + 1 < n).then_some(j + 1);
+                        [
+                            (im1, jm1),
+                            (im1, Some(j)),
+                            (im1, jp1),
+                            (Some(i), jm1),
+                            (Some(i), jp1),
+                            (ip1, jm1),
+                            (ip1, Some(j)),
+                            (ip1, jp1),
+                        ]
+                        .into_iter()
+                        .filter_map(|(i, j)| i.zip(j))
+                        .filter(|&(i, j)| grid[i][j])
+                        .count()
+                    })
+                })
+                .collect()
+        })
+        .collect()
 }
 
 #[cfg(test)]
