1aae26ec0c81d89b951ba840fea8cfe7331b1a81
skookumchoocher <natebrooks004@gmail.com>
2025-12-14 10:37:42 -0600

    refactor(rust/year_2025): day 9

--- rust/year_2025/src/day_09.rs
+++ rust/year_2025/src/day_09.rs
@@ -1,14 +1,7 @@
 use itertools::Itertools as _;
 
 pub fn part_1(input: &str) -> usize {
-    input
-        .lines()
-        .map(|line| {
-            let (x, y) = line.split_once(',').unwrap();
-            let x: usize = x.parse().unwrap();
-            let y: usize = y.parse().unwrap();
-            (x, y)
-        })
+    parse_input(input)
         .tuple_combinations()
         .map(|((x1, y1), (x2, y2))| {
             let dx = x1.abs_diff(x2) + 1;
@@ -20,32 +13,27 @@ pub fn part_1(input: &str) -> usize {
 }
 
 pub fn part_2(input: &str) -> usize {
-    let mut points: Vec<_> = input
-        .lines()
-        .map(|line| {
-            let (x, y) = line.split_once(',').unwrap();
-            let x: usize = x.parse().unwrap();
-            let y: usize = y.parse().unwrap();
-            (x, y)
-        })
-        .collect();
-    points.push(points[0]);
-
-    points[..points.len() - 1]
+    let points: Vec<_> = parse_input(input).collect();
+    points
         .iter()
         .tuple_combinations()
         .filter(|&(&(x1, y1), &(x2, y2))| {
             // Inner rectangle
-            let (rx1, rx2) = (x1.min(x2) + 1, x1.max(x2) - 1);
-            let (ry1, ry2) = (y1.min(y2) + 1, y1.max(y2) - 1);
-            !points.array_windows().any(|&[(tx1, ty1), (tx2, ty2)]| {
-                // Union  between rectangle and tiles
-                let ux1 = tx1.min(tx2).max(rx1);
-                let ux2 = tx1.max(tx2).min(rx2);
-                let uy1 = ty1.min(ty2).max(ry1);
-                let uy2 = ty1.max(ty2).min(ry2);
-                ux1 <= ux2 && uy1 <= uy2
-            })
+            let rx1 = x1.min(x2) + 1;
+            let rx2 = x1.max(x2) - 1;
+            let ry1 = y1.min(y2) + 1;
+            let ry2 = y1.max(y2) - 1;
+            points
+                .iter()
+                .circular_tuple_windows()
+                .all(|(&(tx1, ty1), &(tx2, ty2))| {
+                    // Union  between rectangle and tiles
+                    let ux1 = tx1.min(tx2).max(rx1);
+                    let ux2 = tx1.max(tx2).min(rx2);
+                    let uy1 = ty1.min(ty2).max(ry1);
+                    let uy2 = ty1.max(ty2).min(ry2);
+                    ux1 > ux2 || uy1 > uy2
+                })
         })
         .map(|(&(x1, y1), &(x2, y2))| {
             let dx = x1.abs_diff(x2) + 1;
@@ -54,6 +42,13 @@ pub fn part_2(input: &str) -> usize {
         })
         .max()
         .unwrap()
+}
+
+fn parse_input(input: &str) -> impl Iterator<Item = (usize, usize)> + Clone {
+    input.lines().map(|line| {
+        let (x, y) = line.split_once(',').unwrap();
+        (x.parse().unwrap(), y.parse().unwrap())
+    })
 }
 
 #[cfg(test)]
