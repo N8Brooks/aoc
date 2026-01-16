de448676a47e55bac3444eaed983cf301eb251ef
skookumchoocher <natebrooks004@gmail.com>
2026-01-16 11:10:52 -0600

    refactor(rust/year_2018): day 4

--- rust/year_2018/src/day_04.rs
+++ rust/year_2018/src/day_04.rs
@@ -1,9 +1,35 @@
 use std::collections::HashMap;
 
-use chrono::NaiveDateTime;
 use itertools::Itertools;
-use lazy_static::lazy_static;
-use regex::Regex;
+
+pub fn part_1(input: &str) -> usize {
+    let (id, counts) = parse_input(input)
+        .into_iter()
+        .max_by_key(|(_, counts)| counts.iter().sum::<usize>())
+        .unwrap();
+    let (minute, _) = counts
+        .into_iter()
+        .enumerate()
+        .max_by_key(|(_, count)| *count)
+        .unwrap();
+    id * minute
+}
+
+pub fn part_2(input: &str) -> usize {
+    let (id, minute, _) = parse_input(input)
+        .into_iter()
+        .map(|(id, counts)| {
+            let (minute, count) = counts
+                .into_iter()
+                .enumerate()
+                .max_by_key(|(_, count)| *count)
+                .unwrap();
+            (id, minute, count)
+        })
+        .max_by_key(|(.., count)| *count)
+        .unwrap();
+    id * minute
+}
 
 #[derive(Debug, PartialEq, Eq)]
 enum Event {
@@ -12,88 +38,53 @@ enum Event {
     WakeUp,
 }
 
+use Event::*;
+
 fn parse_input(input: &str) -> HashMap<usize, [usize; 60]> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r"(?m)^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2})\] (?P<event>falls asleep|Guard #(?P<guard_id>\d+) begins shift|wakes up)$").unwrap();
-    }
-    RE.captures_iter(input)
-        .map(|caps| {
-            let time = {
-                NaiveDateTime::parse_from_str(&caps[1], "%Y-%m-%d %H:%M")
-                    .unwrap()
-                    .and_utc()
-                    .timestamp()
-                    / 60
+    input
+        .lines()
+        .map(|line| {
+            let (timestamp, event) = (&line[..18], &line[19..]);
+            let timestamp = timestamp.strip_circumfix('[', ']').unwrap();
+            let event = if event == "falls asleep" {
+                FallAsleep
+            } else if event == "wakes up" {
+                WakeUp
+            } else if let Some(id) = event.strip_circumfix("Guard #", " begins shift") {
+                BeginShift(id.parse().unwrap())
+            } else {
+                panic!("unknown event: {}", event);
             };
-            let event = {
-                let event_name = caps.name("event").unwrap().as_str();
-                let guard_id = caps.name("guard_id");
-                match (event_name, guard_id) {
-                    ("falls asleep", None) => Event::FallAsleep,
-                    ("wakes up", None) => Event::WakeUp,
-                    (_, Some(guard_id)) => Event::BeginShift(guard_id.as_str().parse().unwrap()),
-                    _ => panic!("unknown event"),
-                }
-            };
-            (time, event)
+            (timestamp, event)
         })
-        .sorted_unstable_by_key(|(time, _)| *time)
-        .scan(None, |guard_id, (time, event)| {
-            if let Event::BeginShift(new_guard_id) = event {
-                *guard_id = Some(new_guard_id);
+        .sorted_unstable_by(|(a, _), (b, _)| a.cmp(b))
+        .scan(None, |id, (timestamp, event)| {
+            if let BeginShift(id2) = event {
+                *id = Some(id2);
             }
-            Some((
-                usize::try_from(time.rem_euclid(60)).unwrap(),
-                guard_id.expect("guard began shift"),
-                event,
-            ))
+            Some((id.expect("guard began shift"), timestamp, event))
         })
-        .chunk_by(|(_, guard_id, event)| {
-            let is_sleep_cycle = event == &Event::WakeUp || event == &Event::FallAsleep;
-            (is_sleep_cycle, *guard_id)
-        })
+        .chunk_by(|(id, ..)| *id)
         .into_iter()
-        .filter_map(|((is_sleep_cycle, guard_id), data)| {
-            is_sleep_cycle.then(|| {
-                let sleep_ranges = data
-                    .map(|(minute, ..)| minute)
-                    .tuples()
-                    .map(|(fall_asleep_minute, wake_up_minute)| fall_asleep_minute..wake_up_minute);
-                (guard_id, sleep_ranges)
-            })
+        .into_grouping_map_by(|(id, _)| *id)
+        .fold([0; 60], |counts, _, (_, data)| {
+            data.filter(|(.., event)| !matches!(event, BeginShift(_)))
+                .map(|(_, timestamp, event)| {
+                    let (_, minute) = timestamp.rsplit_once(':').unwrap();
+                    let minute: usize = minute.parse().unwrap();
+                    (minute, event)
+                })
+                .tuples()
+                .flat_map(|((sleep, event_1), (wake, event_2))| {
+                    assert_eq!(event_1, FallAsleep);
+                    assert_eq!(event_2, WakeUp);
+                    sleep..wake
+                })
+                .fold(counts, |mut counts, minute| {
+                    counts[minute] += 1;
+                    counts
+                })
         })
-        .into_grouping_map_by(|(guard_id, _)| *guard_id)
-        .fold([0; 60], |mut minute_counts, _, (_, sleep_ranges)| {
-            for minute in sleep_ranges.flatten() {
-                minute_counts[minute] += 1;
-            }
-            minute_counts
-        })
-}
-
-pub fn part_1(input: &str) -> usize {
-    let (guard_id, minute_counts) = parse_input(input)
-        .into_iter()
-        .max_by_key(|(_, counts)| counts.iter().sum::<usize>())
-        .unwrap();
-    let minute = (0..60).max_by_key(|minute| minute_counts[*minute]).unwrap();
-    guard_id * minute
-}
-
-pub fn part_2(input: &str) -> usize {
-    let (guard_id, minute, _) = parse_input(input)
-        .into_iter()
-        .map(|(guard_id, minute_counts)| {
-            let (max_minute, count) = minute_counts
-                .into_iter()
-                .enumerate()
-                .max_by_key(|(_, count)| *count)
-                .unwrap();
-            (guard_id, max_minute, count)
-        })
-        .max_by_key(|(.., count)| *count)
-        .unwrap();
-    guard_id * minute
 }
 
 #[cfg(test)]
