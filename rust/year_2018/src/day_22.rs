eaddacfef55390de921d00b384f1158dc98d3aad
skookumchoocher <natebrooks004@gmail.com>
2026-01-17 09:50:56 -0600

    refactor(rust/year_2018): day 22

--- rust/year_2018/src/day_22.rs
+++ rust/year_2018/src/day_22.rs
@@ -1,47 +1,48 @@
 use std::{cmp::Reverse, collections::BinaryHeap, iter::successors, mem};
 
+type Region = usize;
+
+type Gear = usize;
+
 pub fn part_1(input: &str) -> usize {
     let (depth, target) = parse_input(input);
-    parse_map(depth, target, target)
-        .into_iter()
-        .flatten()
-        .map(|region| region as usize)
-        .sum()
+    parse_map(depth, target, target).into_iter().flatten().sum()
 }
 
 pub fn part_2(input: &str) -> usize {
-    use Gear::Torch;
-    let (depth, target @ (n, m)) = parse_input(input);
-    let map = parse_map(depth, target, (n + 50, m + 50));
+    const TORCH: Gear = 1;
+    let (depth, target @ (i, j)) = parse_input(input);
+    let (n, m) = (i + 50, j + 50);
+    let map = parse_map(depth, target, (n, m));
 
-    let mut heap = BinaryHeap::from([(Reverse(0), (0, 0), Torch)]);
+    let mut heap = BinaryHeap::from([(Reverse(0), (0, 0), TORCH)]);
     let mut seen = vec![vec![[false; 3]; map[0].len()]; map.len()];
 
     while let Some((Reverse(dist), pos @ (i, j), gear)) = heap.pop() {
-        if pos == (n, m) && gear == Torch {
+        if pos == target && gear == TORCH {
             return dist;
         }
-        if mem::replace(&mut seen[i][j][gear as usize], true) {
+        if mem::replace(&mut seen[i][j][gear], true) {
             continue;
-        }
-
-        let region = map[i][j];
-        let gear_2: Gear = (3 - region as u8 - gear as u8).into();
-        if !seen[i][j][gear_2 as usize] {
-            heap.push((Reverse(dist + 7), pos, gear_2));
         }
 
         [
             i.checked_sub(1).map(|i| (i, j)),
-            (i + 1 < map.len()).then_some((i + 1, j)),
+            (i + 1 < n).then(|| (i + 1, j)),
             j.checked_sub(1).map(|j| (i, j)),
-            (j + 1 < map[0].len()).then_some((i, j + 1)),
+            (j + 1 < m).then(|| (i, j + 1)),
         ]
         .into_iter()
         .flatten()
-        .filter(|&(i, j)| !seen[i][j][gear as usize] && (gear as u8 != map[i][j] as u8))
+        .filter(|&(i, j)| !seen[i][j][gear] && gear != map[i][j])
         .map(|pos| (Reverse(dist + 1), pos, gear))
         .collect_into(&mut heap);
+
+        let region = map[i][j];
+        let gear = 3 - region - gear;
+        if !seen[i][j][gear] {
+            heap.push((Reverse(dist + 7), pos, gear));
+        }
     }
     panic!("no path found");
 }
@@ -49,11 +50,8 @@ pub fn part_2(input: &str) -> usize {
 fn parse_input(input: &str) -> (usize, (usize, usize)) {
     let (depth, target) = input.split_once('\n').unwrap();
     let depth = depth.strip_prefix("depth: ").unwrap().parse().unwrap();
-    let (m, n) = target
-        .strip_prefix("target: ")
-        .unwrap()
-        .split_once(',')
-        .unwrap();
+    let target = target.strip_prefix("target: ").unwrap();
+    let (m, n) = target.split_once(',').unwrap();
     let n = n.parse().unwrap();
     let m = m.parse().unwrap();
     (depth, (n, m))
@@ -78,52 +76,8 @@ fn parse_map(depth: usize, target: (usize, usize), (n, m): (usize, usize)) -> Ve
         Some(row)
     })
     .take(n + 1)
-    .map(|row| {
-        row.into_iter()
-            .map(|level| (level % 3).into())
-            .collect::<Vec<_>>()
-    })
+    .map(|row| row.into_iter().map(|level| level % 3).collect::<Vec<_>>())
     .collect()
-}
-
-#[repr(u8)]
-#[derive(Copy, Clone)]
-enum Region {
-    Rocky = 0,
-    Wet = 1,
-    Narrow = 2,
-}
-
-impl From<usize> for Region {
-    fn from(region: usize) -> Self {
-        use Region::*;
-        match region {
-            0 => Rocky,
-            1 => Wet,
-            2 => Narrow,
-            _ => panic!("invalid region {region}"),
-        }
-    }
-}
-
-#[repr(u8)]
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-enum Gear {
-    Neither = 0,
-    Torch = 1,
-    Climbing = 2,
-}
-
-impl From<u8> for Gear {
-    fn from(gear: u8) -> Self {
-        use Gear::*;
-        match gear {
-            0 => Neither,
-            1 => Torch,
-            2 => Climbing,
-            _ => panic!("invalid gear {gear}"),
-        }
-    }
 }
 
 #[cfg(test)]
