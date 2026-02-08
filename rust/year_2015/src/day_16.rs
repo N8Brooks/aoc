8f71b8599cd2b6bd07f123cf3db3b946d2425f41
skookumchoocher <natebrooks004@gmail.com>
2026-02-08 17:14:20 -0600

    feat(rust/year_2015): day 16

---
+++ rust/year_2015/src/day_16.rs
@@ -0,0 +1,102 @@
+pub fn part_1(input: &str) -> usize {
+    let predicate = |compounds| {
+        matches!(
+            compounds,
+            Compounds {
+                children: Some(3) | None,
+                cats: Some(7) | None,
+                samoyeds: Some(2) | None,
+                pomeranians: Some(3) | None,
+                akitas: Some(0) | None,
+                vizslas: Some(0) | None,
+                goldfish: Some(5) | None,
+                trees: Some(3) | None,
+                cars: Some(2) | None,
+                perfumes: Some(1) | None,
+            }
+        )
+    };
+    find_sue(input, predicate)
+}
+
+pub fn part_2(input: &str) -> usize {
+    let predicate = |compounds| {
+        matches!(
+            compounds,
+            Compounds {
+                children: Some(3) | None,
+                cats: Some(8..) | None,
+                samoyeds: Some(2) | None,
+                pomeranians: Some(..3) | None,
+                akitas: Some(0) | None,
+                vizslas: Some(0) | None,
+                goldfish: Some(..5) | None,
+                trees: Some(4..) | None,
+                cars: Some(2) | None,
+                perfumes: Some(1) | None,
+            }
+        )
+    };
+    find_sue(input, predicate)
+}
+
+fn find_sue(input: &str, test: impl Fn(Compounds) -> bool) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let mut compounds = Compounds::default();
+            let (_sue, parts) = line.split_once(": ").unwrap();
+            for part in parts.split(", ") {
+                let (name, value) = part.split_once(": ").unwrap();
+                let entry = match name {
+                    "children" => &mut compounds.children,
+                    "cats" => &mut compounds.cats,
+                    "samoyeds" => &mut compounds.samoyeds,
+                    "pomeranians" => &mut compounds.pomeranians,
+                    "akitas" => &mut compounds.akitas,
+                    "vizslas" => &mut compounds.vizslas,
+                    "goldfish" => &mut compounds.goldfish,
+                    "trees" => &mut compounds.trees,
+                    "cars" => &mut compounds.cars,
+                    "perfumes" => &mut compounds.perfumes,
+                    _ => panic!("unknown compound {name}"),
+                };
+                *entry = Some(value.parse().unwrap());
+            }
+            compounds
+        })
+        .position(test)
+        .unwrap()
+        + 1
+}
+
+#[derive(Default)]
+struct Compounds {
+    children: Option<usize>,
+    cats: Option<usize>,
+    samoyeds: Option<usize>,
+    pomeranians: Option<usize>,
+    akitas: Option<usize>,
+    vizslas: Option<usize>,
+    goldfish: Option<usize>,
+    trees: Option<usize>,
+    cars: Option<usize>,
+    perfumes: Option<usize>,
+}
+
+#[cfg(test)]
+mod test {
+    use test_case::test_case;
+
+    const INPUT: &str = include_str!("../test_data/day_16.txt");
+
+    #[test_case(INPUT => 213)]
+    fn part_1(input: &str) -> usize {
+        super::part_1(input)
+    }
+
+    #[test_case(INPUT => 323)]
+    fn part_2(input: &str) -> usize {
+        super::part_2(input)
+    }
+}
