bdffd88dc9f8ad767745ca59da7c3cc7f043757e
skookumchoocher <natebrooks004@gmail.com>
2026-02-10 10:42:57 -0600

    refactor(rust/year_2015): day 16

--- rust/year_2015/src/day_16.rs
+++ rust/year_2015/src/day_16.rs
@@ -1,73 +1,72 @@
 pub fn part_1(input: &str) -> usize {
-    let predicate = |compounds| {
-        matches!(
-            compounds,
-            Compounds {
-                children: Some(3) | None,
-                cats: Some(7) | None,
-                samoyeds: Some(2) | None,
-                pomeranians: Some(3) | None,
-                akitas: Some(0) | None,
-                vizslas: Some(0) | None,
-                goldfish: Some(5) | None,
-                trees: Some(3) | None,
-                cars: Some(2) | None,
-                perfumes: Some(1) | None,
-            }
-        )
-    };
-    find_sue(input, predicate)
+    parse_compounds(input)
+        .position(|compounds| {
+            matches!(
+                compounds,
+                Compounds {
+                    children: Some(3) | None,
+                    cats: Some(7) | None,
+                    samoyeds: Some(2) | None,
+                    pomeranians: Some(3) | None,
+                    akitas: Some(0) | None,
+                    vizslas: Some(0) | None,
+                    goldfish: Some(5) | None,
+                    trees: Some(3) | None,
+                    cars: Some(2) | None,
+                    perfumes: Some(1) | None,
+                }
+            )
+        })
+        .unwrap()
+        + 1
 }
 
 pub fn part_2(input: &str) -> usize {
-    let predicate = |compounds| {
-        matches!(
-            compounds,
-            Compounds {
-                children: Some(3) | None,
-                cats: Some(8..) | None,
-                samoyeds: Some(2) | None,
-                pomeranians: Some(..3) | None,
-                akitas: Some(0) | None,
-                vizslas: Some(0) | None,
-                goldfish: Some(..5) | None,
-                trees: Some(4..) | None,
-                cars: Some(2) | None,
-                perfumes: Some(1) | None,
-            }
-        )
-    };
-    find_sue(input, predicate)
-}
-
-fn find_sue(input: &str, test: impl Fn(Compounds) -> bool) -> usize {
-    input
-        .lines()
-        .map(|line| {
-            let mut compounds = Compounds::default();
-            let (_sue, parts) = line.split_once(": ").unwrap();
-            for part in parts.split(", ") {
-                let (name, value) = part.split_once(": ").unwrap();
-                let entry = match name {
-                    "children" => &mut compounds.children,
-                    "cats" => &mut compounds.cats,
-                    "samoyeds" => &mut compounds.samoyeds,
-                    "pomeranians" => &mut compounds.pomeranians,
-                    "akitas" => &mut compounds.akitas,
-                    "vizslas" => &mut compounds.vizslas,
-                    "goldfish" => &mut compounds.goldfish,
-                    "trees" => &mut compounds.trees,
-                    "cars" => &mut compounds.cars,
-                    "perfumes" => &mut compounds.perfumes,
-                    _ => panic!("unknown compound {name}"),
-                };
-                *entry = Some(value.parse().unwrap());
-            }
-            compounds
+    parse_compounds(input)
+        .position(|compounds| {
+            matches!(
+                compounds,
+                Compounds {
+                    children: Some(3) | None,
+                    cats: Some(8..) | None,
+                    samoyeds: Some(2) | None,
+                    pomeranians: Some(..3) | None,
+                    akitas: Some(0) | None,
+                    vizslas: Some(0) | None,
+                    goldfish: Some(..5) | None,
+                    trees: Some(4..) | None,
+                    cars: Some(2) | None,
+                    perfumes: Some(1) | None,
+                }
+            )
         })
-        .position(test)
         .unwrap()
         + 1
+}
+
+fn parse_compounds(input: &str) -> impl Iterator<Item = Compounds> {
+    input.lines().map(|line| {
+        let mut compounds = Compounds::default();
+        let (_sue, parts) = line.split_once(": ").unwrap();
+        for part in parts.split(", ") {
+            let (name, value) = part.split_once(": ").unwrap();
+            let entry = match name {
+                "children" => &mut compounds.children,
+                "cats" => &mut compounds.cats,
+                "samoyeds" => &mut compounds.samoyeds,
+                "pomeranians" => &mut compounds.pomeranians,
+                "akitas" => &mut compounds.akitas,
+                "vizslas" => &mut compounds.vizslas,
+                "goldfish" => &mut compounds.goldfish,
+                "trees" => &mut compounds.trees,
+                "cars" => &mut compounds.cars,
+                "perfumes" => &mut compounds.perfumes,
+                _ => panic!("unknown compound {name}"),
+            };
+            *entry = Some(value.parse().unwrap());
+        }
+        compounds
+    })
 }
 
 #[derive(Default)]
