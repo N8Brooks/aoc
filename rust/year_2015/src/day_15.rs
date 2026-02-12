ba9bf0cdf19424d1a1b7d891524d1755fb4f1ffe
skookumchoocher <natebrooks004@gmail.com>
2026-02-12 11:43:46 -0600

    refactor(rust/year_2015): day 15

--- rust/year_2015/src/day_15.rs
+++ rust/year_2015/src/day_15.rs
@@ -5,19 +5,18 @@ use itertools::Itertools as _;
 pub fn part_1(input: &str) -> u32 {
     let ingredients = parse_ingredients(input);
     allocations(100, ingredients.len())
-        .filter_map(|allocation| {
-            let properties = ingredients.iter().zip(allocation).fold(
-                [0; 4],
-                |mut properties, (ingredient, amount)| {
-                    for (prop, &value) in properties.iter_mut().zip(ingredient) {
+        .map(|allocation| {
+            ingredients
+                .iter()
+                .zip(allocation)
+                .fold([0; 4], |mut props, (ingredient, amount)| {
+                    for (prop, &value) in props.iter_mut().zip(ingredient) {
                         *prop += value * amount as i32;
                     }
-                    properties
-                },
-            );
-            properties
+                    props
+                })
                 .into_iter()
-                .map(|prop| u32::try_from(prop).ok())
+                .map(|prop| u32::try_from(prop).unwrap_or(0))
                 .product()
         })
         .max()
@@ -27,53 +26,49 @@ pub fn part_1(input: &str) -> u32 {
 pub fn part_2(input: &str) -> u32 {
     let ingredients = parse_ingredients(input);
     allocations(100, ingredients.len())
-        .filter_map(|allocation| {
-            let properties = ingredients.iter().zip(allocation).fold(
-                [0; 5],
-                |mut properties, (ingredient, amount)| {
-                    for (prop, &value) in properties.iter_mut().zip(ingredient) {
+        .map(|allocation| {
+            ingredients
+                .iter()
+                .zip(allocation)
+                .fold([0; 5], |mut props, (ingredient, amount)| {
+                    for (prop, &value) in props.iter_mut().zip(ingredient) {
                         *prop += value * amount as i32;
                     }
-                    properties
-                },
-            );
-            if properties[4] != 500 {
-                return None;
-            }
-            properties[..4]
-                .iter()
-                .map(|&prop| u32::try_from(prop).ok())
+                    props
+                })
+        })
+        .filter(|[.., calories]| *calories == 500)
+        .map(|[props @ .., _]| {
+            props
+                .into_iter()
+                .map(|prop| u32::try_from(prop).unwrap_or(0))
                 .product()
         })
         .max()
         .unwrap()
 }
 
-fn allocations(r: u32, n: usize) -> impl Iterator<Item = Vec<u32>> {
-    debug_assert!(n > 1);
-    let mut indexes = vec![0; n];
+fn allocations(r: u32, n: usize) -> impl Iterator<Item = [u32; 4]> {
+    debug_assert!(r > 0, "must allocate at least one unit");
+    debug_assert!(n > 1, "must allocate to at least two n");
+    debug_assert!(n <= 4, "hardcoded to support up to four n");
+    let mut indexes = [0; 4];
     indexes[n - 1] = r;
-    let mut state = None;
+    let mut i = n - 2;
     iter::from_fn(move || {
-        if let Some(i) = state {
-            if indexes[i + 1] > 0 {
-                indexes[i] += 1;
-                indexes[i + 1] -= 1;
-                return Some(indexes.clone());
-            } else {
-                if i == 0 {
-                    return None;
-                }
-                for j in i..n - 1 {
-                    indexes[n - 1] += indexes[j];
-                    indexes[j] = 0;
-                }
-                indexes[i - 1] += 1;
-                indexes[n - 1] -= 1;
-            }
+        if indexes[i + 1] > 0 {
+            indexes[i] += 1;
+            indexes[i + 1] -= 1;
+        } else if i == 0 {
+            return None;
+        } else {
+            indexes[n - 1] += indexes[i..n - 1].iter().sum::<u32>();
+            indexes[i..n - 1].fill(0);
+            indexes[n - 1] -= 1;
+            indexes[i - 1] += 1;
+            i = indexes.array_windows().rposition(|[a, b]| a != b).unwrap();
         }
-        state = indexes.array_windows().rposition(|[a, b]| a != b);
-        Some(indexes.clone())
+        Some(indexes)
     })
 }
 
@@ -81,8 +76,8 @@ fn parse_ingredients(input: &str) -> Vec<[i32; 5]> {
     input
         .lines()
         .map(|line| {
-            let (_, properties) = line.split_once(": ").unwrap();
-            properties
+            let (_, props) = line.split_once(": ").unwrap();
+            props
                 .split(", ")
                 .map(|property| {
                     let (_, value) = property.split_once(' ').unwrap();
