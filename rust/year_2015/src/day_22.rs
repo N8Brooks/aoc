4f8b4f4225e06c73af61798db00c6eec7602f3fc
skookumchoocher <natebrooks004@gmail.com>
2026-02-20 15:45:05 -0600

    refactor(rust/year_2015): day 22

--- rust/year_2015/src/day_22.rs
+++ rust/year_2015/src/day_22.rs
@@ -35,9 +35,9 @@ struct State {
     hp_1: NonZeroU32,
     hp_2: Option<NonZeroU32>,
     mana: u32,
-    shield: u32,
-    poison: u32,
-    recharge: u32,
+    shield: Option<NonZeroU32>,
+    poison: Option<NonZeroU32>,
+    recharge: Option<NonZeroU32>,
 }
 
 impl State {
@@ -48,9 +48,9 @@ impl State {
             hp_1: NonZeroU32::new(50).unwrap(),
             hp_2: NonZeroU32::new(hp_2),
             mana: 500,
-            shield: 0,
-            poison: 0,
-            recharge: 0,
+            shield: None,
+            poison: None,
+            recharge: None,
         }
     }
 
@@ -64,37 +64,42 @@ impl State {
             .hp_2
             .unwrap()
             .get()
-            .saturating_sub(if self.poison > 0 { 3 } else { 0 });
-        let mana = self.mana + if self.recharge > 0 { 101 } else { 0 };
-        let shield_1 = self.shield.saturating_sub(1);
-        let poison_1 = self.poison.saturating_sub(1);
-        let recharge_1 = self.recharge.saturating_sub(1);
+            .saturating_sub(self.poison.map_or(0, |_| 3));
+        let mana = self.mana + self.recharge.map_or(0, |_| 101);
+        let shield_1 = decrement(self.shield);
+        let poison_1 = decrement(self.poison);
+        let recharge_1 = decrement(self.recharge);
 
         // Spell effects
         let res = [
-            (53, 4, 0, 0, 0, 0),
-            (73, 2, 2, 0, 0, 0),
-            (113, 0, 0, 6, 0, 0),
-            (173, 0, 0, 0, 6, 0),
-            (229, 0, 0, 0, 0, 5),
+            (53, 4, 0, None, None, None),
+            (73, 2, 2, None, None, None),
+            (113, 0, 0, NonZeroU32::new(6), None, None),
+            (173, 0, 0, None, NonZeroU32::new(6), None),
+            (229, 0, 0, None, None, NonZeroU32::new(5)),
         ]
         .into_iter()
-        .filter(move |&(cost, _, _, shield_2, poison_2, recharge_2)| {
-            cost <= mana
-                && (shield_1 == 0 || shield_2 == 0)
-                && (poison_1 == 0 || poison_2 == 0)
-                && (recharge_1 == 0 || recharge_2 == 0)
-        })
-        .map(
-            move |(cost, damage, heal, shield_2, poison_2, recharge_2)| State {
-                spent: Reverse(spent + cost),
-                turn: false,
-                hp_1: (hp_1.get() + heal).try_into().unwrap(),
-                hp_2: hp_2.saturating_sub(damage).try_into().ok(),
-                mana: mana - cost,
-                shield: shield_1 | shield_2,
-                poison: poison_1 | poison_2,
-                recharge: recharge_1 | recharge_2,
+        .filter_map(
+            move |(cost, damage, heal, shield_2, poison_2, recharge_2)| {
+                Some(State {
+                    spent: Reverse(spent + cost),
+                    turn: false,
+                    hp_1: (hp_1.get() + heal).try_into().unwrap(),
+                    hp_2: hp_2.saturating_sub(damage).try_into().ok(),
+                    mana: mana.checked_sub(cost)?,
+                    shield: match (shield_1, shield_2) {
+                        (Some(_), Some(_)) => return None,
+                        _ => shield_1.or(shield_2),
+                    },
+                    poison: match (poison_1, poison_2) {
+                        (Some(_), Some(_)) => return None,
+                        _ => poison_1.or(poison_2),
+                    },
+                    recharge: match (recharge_1, recharge_2) {
+                        (Some(_), Some(_)) => return None,
+                        _ => recharge_1.or(recharge_2),
+                    },
+                })
             },
         );
 
@@ -106,11 +111,11 @@ impl State {
             .hp_2
             .unwrap()
             .get()
-            .saturating_sub(if self.poison > 0 { 3 } else { 0 })
+            .saturating_sub(self.poison.map_or(0, |_| 3))
             .try_into()
             .ok();
         let attack = if hp_2.is_some() {
-            let armor = if self.shield > 0 { 7 } else { 0 };
+            let armor = self.shield.map_or(0, |_| 7);
             damage_2.saturating_sub(armor).max(1)
         } else {
             0
@@ -125,12 +130,18 @@ impl State {
                 .try_into()
                 .ok()?,
             hp_2,
-            mana: (self.mana + if self.recharge > 0 { 101 } else { 0 }),
-            shield: self.shield.saturating_sub(1),
-            poison: self.poison.saturating_sub(1),
-            recharge: self.recharge.saturating_sub(1),
+            mana: self.mana + self.recharge.map_or(0, |_| 101),
+            shield: decrement(self.shield),
+            poison: decrement(self.poison),
+            recharge: decrement(self.recharge),
         })
     }
+}
+
+#[inline(always)]
+fn decrement(x: Option<NonZeroU32>) -> Option<NonZeroU32> {
+    x.and_then(|x| x.get().checked_sub(1))
+        .and_then(|x| x.try_into().ok())
 }
 
 fn parse_boss(input: &str) -> (u32, u32) {
