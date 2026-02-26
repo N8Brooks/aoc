68d636d9dbb61f1fd2aa7cd63c7cd8f4d70469f5
skookumchoocher <natebrooks004@gmail.com>
2026-02-26 10:30:19 -0600

    refactor(rust/year_2019): day 11

--- rust/year_2019/src/day_11.rs
+++ rust/year_2019/src/day_11.rs
@@ -2,41 +2,15 @@ use std::{cell::Cell, iter::repeat_with, rc::Rc};
 
 use hashbrown::HashMap;
 use itertools::Itertools as _;
-use num::Complex;
 
 use crate::intcode::{IntcodeExt as _, parse_program};
 
 pub fn part_1(input: &str) -> usize {
-    paint_panels(input, false).len()
-}
-
-pub fn part_2(input: &str) -> String {
-    let painted = paint_panels(input, true);
-    let (min_j, max_j) = painted.keys().map(|p| p.re).minmax().into_option().unwrap();
-    let (min_i, max_i) = painted.keys().map(|p| p.im).minmax().into_option().unwrap();
-    (min_i..=max_i)
-        .rev()
-        .map(|y| -> String {
-            (min_j..=max_j)
-                .map(|x| {
-                    let p = Complex::new(x, y);
-                    let c = painted.get(&p).copied().unwrap_or(false);
-                    if c { '#' } else { ' ' }
-                })
-                .collect()
-        })
-        .join("\n")
-}
-
-fn paint_panels(input: &str, init: bool) -> HashMap<Complex<isize>, bool> {
-    const R: Complex<isize> = Complex::new(0, -1);
-    const L: Complex<isize> = Complex::new(0, 1);
+    let (mut di, mut dj) = (-1, 0);
+    let (mut i, mut j) = (0, 0);
     let mut painted = HashMap::new();
-    let mut dir = Complex::new(0, 1);
-    let mut loc = Complex::new(0, 0);
-    painted.insert(loc, init);
 
-    let color = Rc::new(Cell::new(init));
+    let color = Rc::new(Cell::new(false));
     let inputs = {
         let color = Rc::clone(&color);
         repeat_with(move || color.get() as isize)
@@ -47,13 +21,47 @@ fn paint_panels(input: &str, init: bool) -> HashMap<Complex<isize>, bool> {
         .map(|output| output != 0)
         .tuples()
         .for_each(|(new_color, right)| {
-            painted.insert(loc, new_color);
-            dir *= if right { R } else { L };
-            loc += dir;
-            color.set(painted.get(&loc).copied().unwrap_or(false));
+            painted.insert((i, j), new_color);
+            (di, dj) = if right { (dj, -di) } else { (-dj, di) };
+            i += di;
+            j += dj;
+            color.set(painted.get(&(i, j)).is_some_and(|&b| b));
+        });
+
+    painted.len()
+}
+
+pub fn part_2(input: &str) -> String {
+    let (mut di, mut dj) = (-1, 0);
+    let (mut i, mut j) = (0, 0);
+    let mut painted = [[b' '; 43]; 6];
+    painted[i][j] = b'#';
+
+    let color = Rc::new(Cell::new(b'#'));
+    let inputs = {
+        let color = Rc::clone(&color);
+        repeat_with(move || (color.get() == b'#') as isize)
+    };
+
+    inputs
+        .intcode(parse_program(input))
+        .map(|output| output != 0)
+        .tuples()
+        .for_each(|(new_color, right)| {
+            painted[i][j] = if new_color { b'#' } else { b' ' };
+            (di, dj) = if right { (dj, -di) } else { (-dj, di) };
+            (i, j) = (i.strict_add_signed(di), j.strict_add_signed(dj));
+            color.set(painted[i][j]);
         });
 
     painted
+        .each_ref()
+        .into_iter()
+        .map(|row| row.as_slice())
+        .intersperse(b"\n")
+        .flatten()
+        .map(|&b| b as char)
+        .collect()
 }
 
 #[cfg(test)]
